ec6f37aa99047ee64349f7cac78fc6be983be8b9
treby <treby@atelier-nodoka.net>
2014-01-16 11:12:25 +0900

    Implemented URI writing feature.

--- src/jp/atndk/android/fuknfc2014/WriteActivity.java
+++ src/jp/atndk/android/fuknfc2014/WriteActivity.java
@@ -1,22 +1,111 @@
 package jp.atndk.android.fuknfc2014;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.nfc.FormatException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class WriteActivity extends Activity implements OnClickListener {
+
+    private NfcAdapter mNfcAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
         
         Button btn_write = (Button) findViewById(R.id.button1);
         btn_write.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button1) {
+            enableSearchTag();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        disableSearchTag();
+    }
+
+    private void enableSearchTag() {
+        Toast.makeText(this, "Searching", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, this.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+        mNfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Toast.makeText(this, intent.getAction(), Toast.LENGTH_LONG).show();
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        writeTag(tag);
+    }
+
+    public void writeTag(Tag tag) {
+        try {
+            List<String> techList = Arrays.asList(tag.getTechList());
+            if (techList.contains(Ndef.class.getName())) {
+                Ndef ndef = Ndef.get(tag);
+                writeNdef(ndef);
+            }
+        } catch (RuntimeException e) {
+            Toast.makeText(this, "Failure", Toast.LENGTH_SHORT);
+        }
+        Toast.makeText(this, "Complete!!", Toast.LENGTH_LONG).show();
+    }
+
+    public void writeNdef(Ndef ndef) {
+        // EditText etWrite = (EditText) findViewById(R.id.editText1);
+        // String writeText = etWrite.getText().toString();
+        
+        NdefRecord record = NdefRecord.createUri("http://www.atelier-nodoka.net/");
+        NdefMessage msg = new NdefMessage(new NdefRecord[] {
+                record
+        });
+
+        try {
+            ndef.connect();
+            ndef.writeNdefMessage(msg);
+        } catch (IOException e) {
+            throw new RuntimeException("failure", e);
+        } catch (FormatException e) {
+            throw new RuntimeException("Format failure", e);
+        } finally {
+            try {
+                ndef.close();
+            } catch (IOException e) {
+            }
+        }
+        
+    }
+    
+    private void disableSearchTag() {
+        mNfcAdapter.disableForegroundDispatch(this);
+    }
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -24,10 +113,4 @@ public class WriteActivity extends Activity implements OnClickListener {
         getMenuInflater().inflate(R.menu.write, menu);
         return true;
     }
-
-    @Override
-    public void onClick(View v) {
-        
-    }
-
 }
