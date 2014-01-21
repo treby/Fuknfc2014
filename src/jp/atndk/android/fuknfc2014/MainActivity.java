caea1d256613b1aa6d84025eff6a8beabbba96da
treby <treby@atelier-nodoka.net>
2014-01-22 01:19:46 +0900

    Added a pronama voice.

--- src/jp/atndk/android/fuknfc2014/MainActivity.java
+++ src/jp/atndk/android/fuknfc2014/MainActivity.java
@@ -7,22 +7,37 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener {
-
+    private PronamaVoiceManager mVoiceManager;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mVoiceManager = new PronamaVoiceManager(this);
+        
         // set listeners
+        ImageView img_pronama = (ImageView) findViewById(R.id.imageView1);
+        img_pronama.setOnClickListener(this);
         Button btn_read = (Button) findViewById(R.id.button1);
         btn_read.setOnClickListener(this);
         Button btn_write = (Button) findViewById(R.id.button2);
         btn_write.setOnClickListener(this);
+        
     }
 
+    @Override
+    protected void onDestroy() {
+        if (mVoiceManager != null) {
+            mVoiceManager.release();
+            mVoiceManager = null;
+        }
+    }
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -43,6 +58,9 @@ public class MainActivity extends Activity implements OnClickListener {
         case R.id.button2:
             intent = new Intent(this, WriteActivity.class);
             startActivity(intent);
+            break;
+        case R.id.imageView1:
+            mVoiceManager.play(PronamaVoiceManager.KEI_VOICE_029, 100);
             break;
         default:
             Toast.makeText(this, "Nothing hit.", Toast.LENGTH_LONG).show();
---
+++ src/jp/atndk/android/fuknfc2014/PronamaVoiceManager.java
@@ -0,0 +1,41 @@
+package jp.atndk.android.fuknfc2014;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.SoundPool;
+
+public class PronamaVoiceManager {
+    public static final int KEI_VOICE_029 = 0;
+    
+    private static final int[] VoiceList = {
+        R.raw.kei_voice_029
+    };
+    
+    private SoundPool mSoundPool;
+    
+    private int mVoiceTable[] = new int[VoiceList.length];
+    
+    public PronamaVoiceManager(Context context) {
+        mSoundPool = new SoundPool(VoiceList.length, AudioManager.STREAM_MUSIC, 0);
+        
+        for (int i = 0; i < VoiceList.length; i++) {
+            mVoiceTable[i] = mSoundPool.load(context, VoiceList[i], 1);
+        }
+    }
+    
+    public void play(int no, int vol) {
+        if (no < 0 || no >= mVoiceTable.length) {
+            return;
+        }
+        
+        float fvol = vol / 100;
+        mSoundPool.play(mVoiceTable[no], fvol, fvol, 0, 0, 1.0f);
+    }
+    
+    public void release() {
+        for (int i = 0; i < mVoiceTable.length; i++) {
+            mSoundPool.unload(mVoiceTable[i]);
+        }
+        mSoundPool.release();
+    }
+}
